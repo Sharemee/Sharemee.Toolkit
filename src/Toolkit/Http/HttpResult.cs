7328e70389adc3fe8f8c81b343786fd44fe19a67
Sharemee <SunTaichengAi@outlook.com>
2024-05-10 18:16:03 +0800

    Create HttpResult.cs
    Http 响应模型, 用于避免重复定义此类模型代码

---
+++ src/Toolkit/Http/HttpResult.cs
@@ -0,0 +1,43 @@
+﻿namespace Sharemee.Toolkit.Http;
+
+/// <summary>
+/// Http 响应数据模型
+/// </summary>
+public class HttpResult
+{
+    /// <summary>
+    /// 服务端返回的请求处理状态
+    /// </summary>
+    /// <remarks>
+    /// 值对应的请求状态可以自定义, 不限于以下列出的一种示例:
+    /// <list type="bullet">
+    ///     <item>0: 请求处理成功</item>
+    ///     <item>1: 请求处理失败</item>
+    /// </list>
+    /// </remarks>
+    public int Code { get; set; }
+
+    /// <summary>
+    /// 服务端返回的消息
+    /// </summary>
+    public string Message { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 服务端返回的数据
+    /// </summary>
+    public object? Data { get; set; }
+
+    /// <summary>
+    /// 时间戳
+    /// </summary>
+    public long Timestamp { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+}
+
+/// <inheritdoc/>
+public class HttpResult<T> : HttpResult
+{
+    /// <summary>
+    /// 服务端返回的数据
+    /// </summary>
+    public new T? Data { get; set; }
+}

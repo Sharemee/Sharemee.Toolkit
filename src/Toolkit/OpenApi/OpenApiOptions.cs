f4431cdeeb4aade8cb7f891956b0cb70f725ae64
Sharemee <SunTaichengAi@outlook.com>
2024-07-07 21:19:01 +0800

    Create OpenApiOptions.cs

---
+++ src/Toolkit/OpenApi/OpenApiOptions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Sharemee.Toolkit.OpenApi;
+
+/// <summary>
+/// 接口信息配置
+/// </summary>
+public class OpenApiOptions
+{
+    /// <summary>
+    /// A short description of the application.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// The version of the OpenAPI document.
+    /// </summary>
+    public string? Version { get; set; }
+
+    /// <summary>
+    /// 接口分组列表
+    /// </summary>
+    public List<OpenApiGroup> OpenApiGroups { get; set; } = [];
+}
+
+/// <summary>
+/// 接口分组信息
+/// </summary>
+public class OpenApiGroup
+{
+    /// <summary>
+    /// REQUIRED. The title of the application.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// A short description of the application.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// The version of the OpenAPI document.
+    /// </summary>
+    public string? Version { get; set; }
+
+    /// <summary>
+    /// 是否是默认分组
+    /// </summary>
+    public bool IsDefault { get; set; }
+
+    public OpenApiInfo GetOpenApiInfo(OpenApiOptions openApiOptions)
+    {
+        return new OpenApiInfo()
+        {
+            Title = Name,
+            Description = Description ?? openApiOptions.Description ?? string.Empty,
+            Version = Version ?? openApiOptions.Version ?? "1.0.0",
+        };
+    }
+}
